ee767edf506ca6fe3b23f07575294d1c1e3bd582
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-09-19 22:02:22 +0200

    even better spongemixins compat

---
+++ src/main/java/ru/timeconqueror/spongemixins/core/SpongeMixinsCore.java
@@ -0,0 +1,12 @@
+package ru.timeconqueror.spongemixins.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * This is here for SpongeMixins backwards compat
+ */
+public class SpongeMixinsCore {
+    public static final String PLUGIN_NAME = "SpongeMixin Core Plugin";
+    public static final Logger LOGGER = LogManager.getLogger(PLUGIN_NAME);
+}
